f711866eac40c294b1d23c0403df845fb245a1a5
Jesús Arias <jesusariasgil1995@hotmail.com>
2018-03-22 09:19:43 +0100

    Modificado el ejercicio4-2

--- ejercicio4-2.py
+++ ejercicio4-2.py
@@ -15,10 +15,30 @@ if r.status_code == 200:
                 codigo=i["entity_id"]
 print ("")
 
+print("ORDENAR POR CRITERIO")
+print("1. Costo")
+print("2. Clasificación")
+print("3. Recientemente agregado")
+opcion=input("Elige el numero de opcion que desea realizar: ")
+print("1. Ascendente")
+print("2. Descendente")
+ord=input("Elige el numero de ordenacion que desea filtrar: ")
+
+if opcion=="1":
+	sort="cost"
+if opcion=="2":
+	sort="rating"
+if opcion=="3":
+	sort="real_distance"
+if ord=="1":
+	order="asc"
+if ord=="2":
+	order="desc"
+
 cont=0
 menu="y"
 while menu == "y":
-        p2={"city_id":codigo, "start":cont, "count":"20"}
+        p2={"city_id":codigo, "start":cont, "count":"20", "sort":sort, "order":order }
         rest=requests.get("https://developers.zomato.com/api/v2.1/search", params=p2, headers=h)
         if rest.status_code == 200:
                 re=rest.json()
